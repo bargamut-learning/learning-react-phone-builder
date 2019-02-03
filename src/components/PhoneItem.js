5c16010dbe2dbeaef487f213ed9dacb422934839
Paul Petrov <paul@bargamut.ru>
2019-02-03 23:40:50 +0300

    Make accordion, refactoring components -----------------------------------------
    * Make accordion
    * Refactoring components
    * Make PhoneItem component as
      PureComponent. TODO: try to repeat
      this behavior with normal Component!

---
+++ src/components/PhoneItem.js
@@ -0,0 +1,77 @@
+import React, { PureComponent } from 'react';
+
+// If need to watch everything changes, use PureComponent
+// import React, { PureComponent } from 'react';
+// class PhoneItem extends PureComponent {
+class PhoneItem extends PureComponent {
+  constructor (props) {
+    super(props);
+
+    // Инициализация состояния
+    this.state = {
+      count: 0
+    };
+
+    this.incrementCounter = incrementCounter.bind(this);
+  }
+
+  // // Must be commented if use PureComponent
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return (
+  //     this.state.isOpen !== nextState.isOpen ||
+  //     this.state.count !== nextState.count
+  //   );
+  // }
+
+  componentWillMount() { console.log('---', 'Mounting'); }
+
+  // // Только если изменяется кто-то из родителей и могли поменяться props
+  // componentWillReceiveProps(nextProps) {
+  //   console.log('---', 'Will recieve props');
+  //   if (nextProps.defaultOpen !== this.props.defaultOpen)
+  //     this.setState({
+  //       isOpen: nextProps.defaultOpen
+  //     });
+  // }
+
+  // Изменится вне зависимости от того, внутри компонента изменения или у родителя
+  componentWillUpdate() {
+    console.log('---', 'Will update');
+  }
+
+  render() {
+    const {phoneItem, isOpen, onBtnClick} = this.props,
+          style = { width: '50%' },
+          body = isOpen
+            ? (<div className="card-body">{phoneItem.title}</div>)
+            : '';
+
+    let btnCaption = isOpen ? 'Скрыть' : 'Показать';
+
+    return (
+      <div className="card mx-auto" style={style}>
+        <div className="card-header" onClick={this.incrementCounter}>
+          <button className="btn btn-primary btn-lg float-right" onClick={onBtnClick}>
+            {btnCaption}
+          </button>
+
+          <h3>{phoneItem.title}</h3>
+
+          <span>Clicked: {this.state.count}</span>
+        </div>
+
+        {body}
+      </div>
+    );
+  }
+}
+
+function incrementCounter() {
+  console.log(this.state);
+
+  this.setState({
+    count: this.state.count + 1
+  });
+}
+
+export default PhoneItem;
---
+++ src/components/PhoneList/index.js
@@ -0,0 +1,39 @@
+import React, { PureComponent } from 'react';
+import PhoneItem from '../PhoneItem';
+import './style.css';
+
+/**
+ * Component with state
+ */
+class PhoneList extends PureComponent {
+  state = {
+    openItemId : null
+  };
+
+  render() {
+    const phoneElems = this.props.phoneItemsList.map((phoneItem, i) => {
+      return (
+        <li
+          key={phoneItem.id}
+          className="phone-list__item"
+        >
+          <PhoneItem
+            phoneItem={phoneItem}
+            isOpen = {this.state.openItemId === phoneItem.id}
+            onBtnClick={this.openItem.bind(this, phoneItem.id)}
+          />
+        </li>
+      );
+    });
+
+    return (
+      <ul>
+        {phoneElems}
+      </ul>
+    );
+  }
+
+  openItem = openItemId => this.setState({ openItemId });
+}
+
+export default PhoneList;
